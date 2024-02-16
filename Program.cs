db36051beacc39c860263b953a180a76cf154f87
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2024-02-16 16:22:22 -0300

    Salvar2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,31 @@
-﻿Console.WriteLine(@"Em um carro que gasta 12Km/L 
-voce de saber quantos litros de gasolina é gasto 
-após o tempo gasto de viagem(Horas) e a velocidade 
-media durante a mesma(em Km/h).");
+﻿using System.Globalization;
 
+double H;
+double Km;
+double mult;
+double L = 12;
+double result;
+
+Console.WriteLine("");
+Console.WriteLine(@"Em um carro que gasta 12Km/L voce de saber 
+quantos litros de gasolina é gasto após o 
+tempo gasto de viagem(Horas) e a velocidade 
+media durante a mesma(em Km/h).");
+Console.WriteLine("");
 Console.WriteLine(@"Agora em forma de calculo use as horas multiplicada pela 
 distancia dividida pelo gasto de litos do carro");
+Console.WriteLine("");
+Console.WriteLine("Ou seja a (litragem gasta = horas * Km/h / Km/L)");
+Console.WriteLine("");
 
-Console.WriteLine("ou seja litragem gasta = horas * Km / Litros");
+Console.WriteLine("Coloque a quantidade de Horas gasta:");
+H = Convert.ToDouble (Console.ReadLine());
 
-int H;
-int Km;
+Console.WriteLine("Agora coloque a velocidade media em Km/h:");
+Km = Convert.ToDouble (Console.ReadLine());
+
+mult = H * Km;
+
+result = mult / L;
+
+Console.WriteLine(result.ToString("F3"));

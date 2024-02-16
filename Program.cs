b11301b2f7fe8ba88a1633edc25f319473859161
AlexFerJesus <141464221+AlexFerJesus@users.noreply.github.com>
2024-02-16 01:46:04 +0000

    Salvar1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine(@"Em um carro que gasta 12Km/L 
+voce de saber quantos litros de gasolina é gasto 
+após o tempo gasto de viagem(Horas) e a velocidade 
+media durante a mesma(em Km/h).");
+
+Console.WriteLine(@"Agora em forma de calculo use as horas multiplicada pela 
+distancia dividida pelo gasto de litos do carro");
+
+Console.WriteLine("ou seja litragem gasta = horas * Km / Litros");
+
+int H;
+int Km;
